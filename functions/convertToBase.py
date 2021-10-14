26f9129adf9be97521c5d9397221573eee0203cd
dan0102dan <dan0102dan@mail.ru>
2021-10-14 17:44:00 +0300

    added comments

--- functions/convertToBase.py
+++ functions/convertToBase.py
@@ -1,17 +1,19 @@
+# конвертирует число из 10 основание в любое другое (пока не работает с отрицательными числами)
+
 def convertToBase(num, base):
-	bases = list('0123456789ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+	bases = list('0123456789ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz') # записал цифры в разных основаниях
 
 	result = [0]
-	for i in range(1, num + 1):
-		for el in range(0, len(result)):
-			if result[el] + 1 < base:
+	for i in range(num): # запускаем цикл, который выполнится num раз
+		for el in range(len(result)): # перебираем наш промежуточный результат, чтобы добавить к каждому разряду единицу
+			if result[el] + 1 < base: # прибавляем единицу, если это не будет превышать заданное основание
 				result[el] += 1
-				break
+				break # остоналиваем, потому что нам больше не нужно ничего добавлять
 			else:
-				result[el] = 0
-				if el + 1 == len(result):
+				result[el] = 0 # обнуляем текущий разряд и идём дальше
+				if el + 1 == len(result): # если это был последний разряд, то добавляем новый
 					result.append(1)
-	return ''.join(map(lambda i: str(bases[i]), reversed(result)))
+	return ''.join(map(lambda x: str(bases[x]), reversed(result)))
 
 print(
 	convertToBase(int(input('Введите число: ')), int(input('Введите основание числа: ')))
